46ac8839542b79a4eede7ede51adbc2873a34f99
Morteza <Morteza.asadi@live.com>
2024-08-13 21:31:59 +0200

    feat: implement ProperFractions method to calculate proper fractions

--- src/CodeWars/4kyu/NumberOfProperFractions.cs
+++ src/CodeWars/4kyu/NumberOfProperFractions.cs
@@ -4,6 +4,21 @@ public static class NumberOfProperFractions
 {
     public static long ProperFractions(long n)
     {
-        return 0;
+        if (n == 1)
+            return 0;
+
+        var result = n;
+        for (var p = 2; p * p <= n; p++)
+        {
+            if (n % p != 0) continue;
+            while (n % p == 0)
+                n /= p;
+            result -= result / p;
+        }
+
+        if (n > 1)
+            result -= result / n;
+        
+        return result;
     }
 }
